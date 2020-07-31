131957e10d7d15e6c07de489813e7f6ebda72e67
berdyshevol <berdyshevo@gmail.com>
2020-07-31 13:04:39 +0300

    marathon repository tests

--- src/test/java/com/softserve/edu/MarathonRepositoryTest.java
+++ src/test/java/com/softserve/edu/MarathonRepositoryTest.java
@@ -10,11 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.List;
 import java.util.Optional;
 
 @DataJpaTest
 public class MarathonRepositoryTest {
-    Logger logger = LoggerFactory.getLogger(MarathonRepositoryTest.class);
     @Autowired
     private MarathonRepository marathonRepository;
 
@@ -23,13 +23,67 @@ public class MarathonRepositoryTest {
 
     @Test
     public void newMarathonTest() {
+        String newMarathonName = "test marathon";
         Marathon marathon = new Marathon();
-        marathon.setName("test marathon");
+        marathon.setName(newMarathonName);
         int size = marathonRepository.findAll().size();
 
         marathonRepository.save(marathon);
         int newSize = marathonRepository.findAll().size();
 
-        Assertions.assertTrue(newSize - size == 1);
+        Assertions.assertEquals(newSize - size, 1);
+
+        Assertions.assertTrue(
+                marathonRepository.findAll().stream().anyMatch(
+                        m -> m.getName().equals(newMarathonName)
+                )
+        );
+    }
+
+    @Test
+    public void sizeTest() {
+        List<Marathon> marathons = marathonRepository.findAll();
+        Assertions.assertTrue(marathons.size() == 2);
+    }
+
+    @Test
+    public void findAllTest() {
+        String marathonName1 = "Java Online Marathons";
+        String marathonName2 = "Python Online Marathons";
+        List<Marathon> marathons = marathonRepository.findAll();
+        Assertions.assertTrue(marathons.size() == 2);
+
+        Assertions.assertTrue(marathons.stream().anyMatch(m -> m.getName().equals(marathonName1)));
+        Assertions.assertTrue(marathons.stream().anyMatch(m -> m.getName().equals(marathonName2)));
+    }
+
+    @Test
+    public void findByIdTest() {
+        Optional<Marathon> marathon = marathonRepository.findById(100000);
+        Assertions.assertFalse(marathon.isPresent());
+
+        Optional<Marathon> marathon1 = marathonRepository.findById(1);
+        Assertions.assertTrue(marathon1.isPresent());
+
+        Optional<Marathon> marathon2 = marathonRepository.findById(2);
+        Assertions.assertTrue(marathon2.isPresent());
+    }
+
+    @Test
+    public void deleteTest() {
+        long size = marathonRepository.count();
+        Optional<Marathon> marathon1 = marathonRepository.findById(1);
+        Assertions.assertTrue(marathon1.isPresent());
+
+        marathonRepository.delete(marathon1.get());
+        Long newSize = marathonRepository.count();
+
+        Assertions.assertEquals(newSize - size, -1);
+    }
+
+    @Test
+    public void deleteAllTest() {
+        marathonRepository.deleteAll();
+        Assertions.assertEquals(marathonRepository.findAll().size(),0);
     }
 }
